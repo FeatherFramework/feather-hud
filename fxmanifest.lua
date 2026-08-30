fd77c33bba97d571770989f330f3055311074038
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-29 23:13:05 -0700

    Added Stream Assets from Core
    - Moved HUD-specific stream assets out of Core and into Feather HUD
    - Added the HUD stream asset documentation
    - Bumped the resource and UI package versions to `0.2.1`

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ lua54 'yes'
 name 'feather-hud'
 description 'The player HUD for the Feather Framework'
 author 'Feather @Bytesizd'
-version '0.2.0'
+version '0.2.1'
 
 shared_scripts {
     'config.lua'
---
+++ stream/README.md
@@ -0,0 +1,3 @@
+HUD override assets used to suppress selected base-game HUD elements.
+
+Credit: MasNana#8272
